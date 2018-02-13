1133c4ec7d17a50edf787456b6ce715c2c3aedd3
OnlyReFLeX <reflexonly@gmail.com>
2018-02-13 20:37:24 +0300

    fix main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
+require_relative './player'
+require_relative './user'
+require_relative './dealer'
 require_relative './card'
 require_relative './controller'
-require_relative './player'
+require_relative './bank'
 
 controller = Controller.new
 controller.start
